d27a497aee50c95d3aac01dad87ab115cf31dc0a
Bryan Tidwell <btidwell@atiba.com>
2019-04-15 15:50:19 -0500

    new jira aliases

--- aliases/terjira_aliases.sh
+++ aliases/terjira_aliases.sh
@@ -3,6 +3,7 @@ alias jconf='nvim $HOME/aliases/terjira_aliases.sh'
 alias j='jira'
 alias ji='jira issue'
 alias jio='jira issue open'
+alias jls='jira issue ls'
 #jira issue comment
 function jic(){
 	jira issue comment -E $1
