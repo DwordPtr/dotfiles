57f1434454733f815bbb7d87033dfb759d847e0d
Bryan Tidwell <btidwell@atiba.com>
2019-04-16 11:08:39 -0500

    terjira alias updates

--- aliases/terjira_aliases.sh
+++ aliases/terjira_aliases.sh
@@ -2,11 +2,21 @@
 alias jconf='nvim $HOME/aliases/terjira_aliases.sh'
 alias j='jira'
 alias ji='jira issue'
-alias jio='jira issue open'
 alias jls='jira issue ls'
+function jio(){
+	if [ "$#" = "0" ]; then
+		jira issue open `i`
+	else 
+		jira issue open $1
+	fi
+}
 #jira issue comment
-function jic(){
-	jira issue comment -E $1
+function jc(){
+	if [ "$#" = "0" ]; then
+		jira issue comment -E `i`
+	else 
+		jira issue comment -E $1
+	fi
 }
 function jt(){
 	if [ "$#" = "0" ]; then
