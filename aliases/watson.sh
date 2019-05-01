e79c6da30743a582775083147eae527d2b52e993
Bryan Tidwell <btidwell@atiba.com>
2019-05-01 10:35:01 -0500

    add to watson alias

--- aliases/watson.sh
+++ aliases/watson.sh
@@ -8,6 +8,7 @@ function start(){
 	watson start $1
 	git checkout development
 	jira issue take $1
+	jira issue trans $1 "In Progress"
 	git pull origin development
 	git checkout -B feature/"$1"-"$2"
 }
