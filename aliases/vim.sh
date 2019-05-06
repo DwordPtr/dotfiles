e00843c37c2c1a5a925de25b0edf48ffca028b02
Bryan Tidwell <btidwell@atiba.com>
2019-05-06 15:40:21 -0500

    use nvim in vim aliases instead of vim

--- aliases/vim.sh
+++ aliases/vim.sh
@@ -1,6 +1,6 @@
-alias vaconf='vim ~/$HOME/aliases/vim.sh'
+alias vaconf='vim $HOME/aliases/vim.sh'
 function vim_which(){
-	vim `which $1`
+	nvim `which $1`
 }
 #todo add autocomplete
 alias vw='vim_which'
