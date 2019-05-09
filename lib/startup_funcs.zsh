2df8978dac1c08d32af1c2131b71d682422fddb1
Bryan Tidwell <btidwell@atiba.com>
2019-05-09 13:31:18 -0500

    tweak startup funcs

--- lib/startup_funcs.zsh
+++ lib/startup_funcs.zsh
@@ -1,9 +1,9 @@
 # setup functions that help
 # make a zshrc and place_files
 # file put them
-# here so they can be used 
+# here so they can be used
 # anywhere in the zshrc file
-# I have anecdotal evidence 
+# I have anecdotal evidence
 # that they work
 function make_clone_dir(){
 	if [ ! -d "$HOME/$1" ]; then
@@ -11,20 +11,16 @@ function make_clone_dir(){
 	fi
 }
 function make_home_dir(){
-   if [ ! -d "$HOME/$1" ]; then
-   	mkdir -p $HOME/$1
-   fi
+	if [ ! -d "$HOME/$1" ]; then
+		mkdir -p $HOME/$1
+	fi
 }
 function add_aliases(){
-     #don't source documentation
-     if [ $1 = *.md ]; then
-	     return 0
-     fi
-     if [ ! -f $HOME/aliases/$1 ]; then
-     	echo no $1 placed
-     else
-             source $HOME/aliases/$1
-     fi
+	if [ ! -f $HOME/aliases/$1 ]; then
+		echo no $1 placed
+	else
+		source $HOME/aliases/$1
+	fi
 }
 function asource(){
 	source $ALIAS_DIR/$1
@@ -36,11 +32,14 @@ function place_alias(){
 
 function place_all_aliases(){
 	for file in $PWD/aliases/*;
-	do place_alias `basename $file`;
+	do
+	   if [ ! $file = *.md ]; then
+	   	place_alias `basename $file`;
+	   fi
 	done
 }
 function src_all_aliases(){
-	for file in $HOME/aliases/*; 
+	for file in $HOME/aliases/*;
 	do add_aliases `basename $file`;
 	done
 }
