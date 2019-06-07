b90d64b9ffc9f930123486f9294803491d177662
btidwell <btidwell@atiba.com>
2019-06-06 22:11:00 -0500

    add scripts for enabling/disabling native keyboard/mouse on some computer (i3 only)

---
+++ scripts/dkeymouse.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
+xinput set-prop "AT Translated Set 2 keyboard" "Device Enabled" 0
---
+++ scripts/ekeymouse.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 1
+xinput set-prop "AT Translated Set 2 keyboard" "Device Enabled" 1
+
