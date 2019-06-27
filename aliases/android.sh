951e0013d38525e32d4df22baac5d30fbdd1bd86
Bryan Tidwell <bryantidwell@Atiba-MAC-4.cdelightband.net>
2019-06-26 22:37:27 -0500

    add android function to capture screencap

--- aliases/android.sh
+++ aliases/android.sh
@@ -2,3 +2,7 @@ alias aconf='nvim $HOME/aliases/android.sh'
 function kill_emulators(){
 	adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
 }
+function scrot(){
+	adb shell screencap -p /sdcard/screen.png
+        adb pull /sdcard/screen.png $1
+}
