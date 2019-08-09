cd7f2448011a58cc4f617cc42f5ae220c0382867
Bryan Tidwell <bryantidwell@Atiba-MAC-4.cdelightband.net>
2019-08-09 15:16:43 -0500

    add alais for vim remote opening files on path

--- aliases/vim.sh
+++ aliases/vim.sh
@@ -2,5 +2,10 @@ alias vaconf='vim $HOME/aliases/vim.sh'
 function vim_which(){
 	nvim `which $1`
 }
+
+function nvr_which(){
+	nvr `which $1`
+}
 #todo add autocomplete
 alias vw='vim_which'
+alias nvw='nvr_which'
