3f3f8c0dcd42048cb68743251a65cd17286775d5
Bryan Tidwell <btidwell4@my.apsu.edu>
2019-12-13 21:50:58 -0600

    add home monitor script

---
+++ scripts/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-1-3 --off --output HDMI-1-2 --mode 3840x2160 --pos 2200x144 --rotate normal --output HDMI-1-1 --mode 3840x2160 --pos 0x0 --rotate left --output eDP-1-1 --mode 2560x1440 --pos 2160x2304 --rotate normal --output DP-1-3 --off --output DP-1-2 --off --output DP-1-1 --off
