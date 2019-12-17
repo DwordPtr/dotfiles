b3cc0a7e68efb1b9cc412acfa2942bc694d5d2e8
Bryan Tidwell <btidwell4@my.apsu.edu>
2019-12-16 20:48:23 -0600

    add folds to place_files

--- place_files.sh
+++ place_files.sh
@@ -1,18 +1,28 @@
 #! /bin/bash
+#    _____ __  __ ____     ____ ___   ___ 
+#   / ___// / / // __ \   / __ `__ \ / _ \
+#  / /   / /_/ // / / /  / / / / / //  __/
+# /_/    \__,_//_/ /_/  /_/ /_/ /_/ \___/ 
+                                        
+# vim:fdm=marker
+# export variables /source library {{{
 export ALIASES=$HOME/aliases
 export DISTRO=`uname`
 source './lib/startup_funcs.zsh'
-
+# }}}
+# find vscode config dir {{{
 if [ "$DISTRO" = "Darwin" ]; then
         VSCODE_DIR="${HOME}/Library/Application\ Support/Code/User"
 else
         VSCODE_DIR="${HOME}/.config/Code/User"
 fi
+# }}}
+# make dependent directories {{{
 make_home_dir 'bin'
 make_home_dir 'lib'
+# }}}
+# place general dot files {{{
 place_config $PWD/lib/startup_funcs.zsh $HOME/lib/startup_funcs.zsh
-
-#don't ever run me outisde of my dir
 place_config $PWD/ideavimrc $HOME/.ideavimrc
 place_config $PWD/vimrc $HOME/.vimrc
 place_config $PWD/tvrc $HOME/.tvrc
@@ -27,18 +37,23 @@ place_config $PWD/kitty.conf $HOME/.config/kitty/kitty.conf
 place_config $PWD/vscode/settings.json "${VSCODE_DIR}/settings.json"
 place_config $PWD/vscode/keybindings.json "${VSCODE_DIR}/keybindings.json"
 place_config $PWD/nvimrc $HOME/.config/nvim/init.nvim
-
+# }}}
+# macos specific configs {{{
 if [ "$DISTRO" = "Darwin" ]; then
         place_config chunkwm/skhdrc ~/.skhdrc
         place_config chunkwm/chunkwmrc ~/.chunkwmrc
         place_config $PWD/assets/apple.jpg $HOME/Pictures/apple.jpg
 fi
-
+# }}}
+# linux specific configs {{{
 if [ "$DISTRO" != "Darwin" ]; then
         place_config $PWD/i3/config $HOME/.config/i3/config
         place_config $PWD/i3/lock.sh $HOME/bin/lock.sh
         place_config $PWD/rofi $HOME/.config/rofi/config.rasi
         place_config $PWD/assets/ibm.jpg $HOME/Pictures/ibm.jpg
 fi
+# }}}
+# place scripts/aliases {{{
 place_scripts
 place_all_aliases
+# }}}
