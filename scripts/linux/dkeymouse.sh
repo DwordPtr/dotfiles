ec16cabbeffc88bd7a9c3635eadc40ec8f252d7a
Bryan Tidwell <btidwell4@my.apsu.edu>
2020-03-18 21:59:14 -0500

    add ibm nub to mouse enable/disable scrips

--- scripts/linux/dkeymouse.sh
+++ scripts/linux/dkeymouse.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
 xinput set-prop "AT Translated Set 2 keyboard" "Device Enabled" 0
+xinput set-prop "TPPS/2 IBM TrackPoint" "Device Enabled" 0
--- scripts/linux/ekeymouse.sh
+++ scripts/linux/ekeymouse.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 1
 xinput set-prop "AT Translated Set 2 keyboard" "Device Enabled" 1
+xinput set-prop "TPPS/2 IBM TrackPoint" "Device Enabled" 1
 
