32997cd2d2aa52f5a49f9317118a65f555580955
Bryan <bryan@Bryans-MBP.cdelightband.net>
2020-08-05 20:45:39 -0500

    fix vim name

--- place_files.sh
+++ place_files.sh
@@ -36,7 +36,7 @@ place_config $PWD/todo_config $HOME/.todo/config
 place_config $PWD/kitty.conf $HOME/.config/kitty/kitty.conf
 place_config $PWD/vscode/settings.json "${VSCODE_DIR}/settings.json"
 place_config $PWD/vscode/keybindings.json "${VSCODE_DIR}/keybindings.json"
-place_config $PWD/nvimrc $HOME/.config/nvim/init.nvim
+place_config $PWD/nvimrc $HOME/.config/nvim/init.vim
 place_configs $PWD/ranger $HOME/.config/ranger
 # }}}
 # macos specific configs {{{
