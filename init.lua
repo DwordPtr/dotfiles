ab99c736195fc012ebe47cf23275307a5d6ed263
bryan tidwell <bryan.tidwell@samsara.com>
2024-10-18 16:19:49 -0500

    get treesitter configs working

--- init.lua
+++ init.lua
@@ -63,7 +63,69 @@ plugins = {
      config = true,
   },
   'mfussenegger/nvim-jdtls',
-  'nvim-treesitter/nvim-treesitter',
+  {'nvim-treesitter/nvim-treesitter', build= ':TSUpdate', 
+   config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "jsonc", "markdown", "lua", 
+          "vim", "vimdoc", "query", "go", "typescript", "javascript", "html" 
+        },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+          refactor = {
+           highlight_definitions = {
+              enable = true,
+              -- Set to false if you have an `updatetime` of ~100.
+              clear_on_cursor_move = true,
+            },
+            highlight_current_scope = { enable = true },
+          },
+          textobjects = {
+            select = {
+              enable = true,
+
+              -- Automatically jump forward to textobj, similar to targets.vim
+              lookahead = true,
+
+              keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                -- nvim_buf_set_keymap) which plugins like which-key display
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                -- You can also use captures from other query groups like `locals.scm`
+                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+              },
+              -- You can choose the select mode (default is charwise 'v')
+              --
+              -- Can also be a function which gets passed a table with the keys
+              -- * query_string: eg '@function.inner'
+              -- * method: eg 'v' or 'o'
+              -- and should return the mode ('v', 'V', or '<c-v>') or a table
+              -- mapping query_strings to modes.
+              selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'V', -- linewise
+                ['@class.outer'] = '<c-v>', -- blockwise
+              },
+              -- If you set this to `true` (default is `false`) then any textobject is
+              -- extended to include preceding or succeeding whitespace. Succeeding
+              -- whitespace has priority in order to act similarly to eg the built-in
+              -- `ap`.
+              --
+              -- Can also be a function which gets passed a table with the keys
+              -- * query_string: eg '@function.inner'
+              -- * selection_mode: eg 'v'
+              -- and should return true or false
+              include_surrounding_whitespace = true,
+            },
+                },})
+            end},
+  'nvim-treesitter/nvim-treesitter-refactor',
   'nvim-treesitter/nvim-treesitter-textobjects',
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   'github/copilot.vim',
@@ -89,6 +151,20 @@ plugins = {
   "mikavilpas/yazi.nvim",
   {'akinsho/git-conflict.nvim', version = "*", config = true},
   "nat-418/boole.nvim",
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
   "junegunn/fzf.vim",
   config = {
     update_interval = 1000,
@@ -168,6 +244,10 @@ require 'term-edit'.setup {
   prompt_end = '➜ '
 }
 require("ibl").setup()
+require("CopilotChat").setup {
+  debug = true, -- Enable debugging
+  -- See Configuration section for rest
+}
 -- Setup language servers.
 require("nvim-lsp-installer").setup {}
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -264,48 +344,3 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 require('remember')
--- tree sitter text object definitions
-require'nvim-treesitter.configs'.setup {
-  textobjects = {
-    select = {
-      enable = true,
-
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-        -- nvim_buf_set_keymap) which plugins like which-key display
-        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-        -- You can also use captures from other query groups like `locals.scm`
-        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-      },
-      -- You can choose the select mode (default is charwise 'v')
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * method: eg 'v' or 'o'
-      -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      -- mapping query_strings to modes.
-      selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
-      },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding or succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * selection_mode: eg 'v'
-      -- and should return true or false
-      include_surrounding_whitespace = true,
-    },
-  },
-}
