ec58214a40d714dd542b2ab0814acecdc60f0a05
bryan tidwell <bryan.tidwell@samsara.com>
2025-02-12 16:48:11 -0600

    swap out git gutter plugin for one that works better

--- init.lua
+++ init.lua
@@ -207,13 +207,7 @@ plugins = {
   "f-person/auto-dark-mode.nvim",
   "nanotee/zoxide.vim",
   'vladdoster/remember.nvim',
-  {
-    'tanvirtin/vgit.nvim',
-    branch = 'v1.0.x',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
-    event = 'VimEnter',
-    config = function() require("vgit").setup() end,
-  },
+  'lewis6991/gitsigns.nvim',
   "ray-x/go.nvim",
   "nvim-tree/nvim-web-devicons",
   "sindrets/diffview.nvim",
@@ -381,16 +375,134 @@ vim.cmd([[colorscheme gruvbox]])
 vim.keymap.set('n', '<leader>rr', ':source $MYVIMRC<CR>')
 vim.keymap.set('n', '<leader>re', ':tabnew ~/.config/nvim/init.lua<CR>')
 
-vgit = require('vgit')
-vgit.setup()
-vim.keymap.set('n', 'gn', vgit.hunk_down)
-vim.keymap.set('n', 'gp', vgit.hunk_up)
-vim.keymap.set('n', 'gs', vgit.buffer_hunk_stage)
-vim.keymap.set('n', 'gu', vgit.buffer_hunk_reset)
---vim.keymap.set('n', '<leader>gb', vgit.buffer_gutter_blame_preview)
+-- git setup {{{
+require('gitsigns').setup {
+  signs                        = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged                 = {
+    add          = { text = '┃' },
+    change       = { text = '┃' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signs_staged_enable          = true,
+  signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir                 = {
+    follow_files = true
+  },
+  auto_attach                  = true,
+  attach_to_untracked          = false,
+  current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts      = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+    virt_text_priority = 100,
+    use_focus = true,
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  sign_priority                = 6,
+  update_debounce              = 100,
+  status_formatter             = nil,   -- Use default
+  max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+  preview_config               = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+  on_attach                    = function(bufnr)
+    local gitsigns = require('gitsigns')
 
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', 'gn', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ ']c', bang = true })
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', 'gp', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ '[c', bang = true })
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    map('n', 'gs', gitsigns.stage_hunk)
+    map('n', 'gu', gitsigns.reset_hunk)
+
+    map('v', '<leader>hs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('v', 'gu', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('n', 'gS', gitsigns.stage_buffer)
+    map('n', 'gSR', gitsigns.reset_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+    map('n', '<leader>gb', function()
+      gitsigns.blame_line({ full = true })
+    end)
+
+    map('n', '<leader>hd', gitsigns.diffthis)
+
+    map('n', '<leader>hD', function()
+      gitsigns.diffthis('~')
+    end)
+
+    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+    map('n', '<leader>hq', gitsigns.setqflist)
+
+    -- Toggles
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    map('n', '<leader>td', gitsigns.toggle_deleted)
+    map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+    -- Text object
+    map({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
+  end
+}
+--- }}}
 
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+require("telescope").setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-u>"] = false
+      },
+    },
+  }
+}
 vim.keymap.set('n', '<leader>ff', builtin.find_files)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
@@ -476,6 +588,7 @@ require('format-on-save').setup({
     golang = formatters.lsp,
   }
 })
+vim.cmd([[colorscheme gruvbox]])
 -- cool if this works
 if string.len(os.getenv("SSH_CONNECTION")) > 0 then
   if vim.fn.executable("pbcopy") == 0 then
